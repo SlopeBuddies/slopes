909c799d6ec8b275501aa7479847845b9ae38e7c
Stefano Martinengo <stefanomartinengo3@gmail.com>
2017-12-04 15:30:44 -0700

    fixed typo

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ export class Login extends Component {
   render() {
     return (
       <div>
-          <a href={procerss.env.REACT_APP_LOGIN}>
+          <a href={process.env.REACT_APP_LOGIN}>
         <button> LOGIN </button>
         </a>
       </div>
