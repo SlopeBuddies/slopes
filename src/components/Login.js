54ddd7ba31b5cb1a68aca56c5115e096c3403265
timknapp12 <timknapp12@gmail.com>
2017-12-11 15:44:25 -0700

    styling of login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
+import logo2 from '../assets/logo2.png';
+import logo3 from '../assets/logo3.png';
 
 export class Login extends Component {
 
   
   render() {
     return (
+      <div className='login_background'>
       <div className='loginContainer'>
-          <div className='loginBox'>
-          <a href={process.env.REACT_APP_LOGIN}>
-        <button> LOGIN </button>
+      <div className='logo_container'>
+      <span className='login_title'>SlopeBuddies</span>
+      <img className='login_logo' src={logo2}/>
+      </div>
+          {/* <div className='loginBox'> */}
+          <a className='login_button' href={process.env.REACT_APP_LOGIN}>
+        <button className='login_button_text'> LOGIN </button>
         </a>
-        </div>
+        {/* </div> */}
 
         
+      </div>
       </div>
     )
   }
