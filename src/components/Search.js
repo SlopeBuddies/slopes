652a785decb6e3d0e242b3e650aa7260fdd877f4
Stefano Martinengo <stefanomartinengo3@gmail.com>
2017-12-12 12:20:13 -0700

    fixed ternary for search friends and put search input in center for better aesthetic

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import searchIcon from "./../assets/searchicon.png";
-import { findUsers,
+import {
+  findUsers,
   getUserInfo,
   getAllFriends,
   getRequest
- } from "./../ducks/reducer";
- import axios from "axios";
- import { connect } from "react-redux";
- import timer from "./../assets/78834-200.png";
- import { Link } from "react-router-dom";
- 
- export class Search extends Component {
+} from "./../ducks/reducer";
+import axios from "axios";
+import { connect } from "react-redux";
+import timer from "./../assets/78834-200.png";
+import { Link } from "react-router-dom";
+import checkmark from './../assets/checkmark.png'
+
+export class Search extends Component {
   constructor() {
     super();
     this.state = {
@@ -18,11 +20,11 @@ import { findUsers,
     };
     this.keyPress = this.keyPress.bind(this);
   }
- 
+
   componentDidMount() {
     this.props.getUserInfo();
   }
- 
+
   findUsers() {
     axios
       .get(
@@ -36,13 +38,13 @@ import { findUsers,
         });
       });
   }
- 
+
   friendRequest(id) {
     axios
       .post(`/send/friend/request/${this.props.user.user_id}/${id}`)
       .then(alert("Request Sent"));
   }
- 
+
   keyPress(e) {
     if (e.keyCode === 13) {
       this.props.getRequest(this.props.user.user_id);
@@ -69,7 +71,7 @@ import { findUsers,
         }
       }
     }
- 
+
     var mapUsers = this.state.users.map((e, i) => {
       return (
         <div key={e.user_id} className="usersList">
@@ -77,13 +79,16 @@ import { findUsers,
           <Link to={`/profile/${e.user_id}`} ><img className="searchIMG" src={e.profile_picture} /></Link>
           </div>
           <div> {e.first_name}</div>
-          {!homies.includes(e.user_id) ?
+          {!homies.includes(e.user_id) ? 
             <div>
               <button className='usersList' onClick={() => this.friendRequest(e.user_id)}
               > + </button>
             </div>
-          : <div className="timerDiv"> <img alt="pending-request" src={timer} className="searchBTN"/></div>}
-        
+          : <div><img className="checkmark" src = {checkmark} alt="Already Friends"/></div>}
+          {pendingFriendReqs.includes(e.user_id) ?
+         <div className="timerDiv"> <img alt="pending-request" src={timer} className="searchBTN"/></div>
+         : null }
+
    </div>
       );
     });
@@ -92,12 +97,8 @@ import { findUsers,
       <div>
       <div>
           <input ref="search" type="text" onKeyDown={this.keyPress} />
- 
-          <img className="searchIcon" alt="" src={searchIcon} onClick={() => {
-              this.props.getRequest(this.props.user.user_id);
-              this.props.getAllFriends(this.props.user.user_id);
-              this.findUsers();
-            }} />
+
+          <img className="searchIcon" alt="" src={searchIcon} />
  
           </div>
           </div>
@@ -105,15 +106,15 @@ import { findUsers,
       </div>
     );
   }
- }
- 
- function mapStateToProps(state) {
+}
+
+function mapStateToProps(state) {
   return state;
- }
- 
- export default connect(mapStateToProps, {
+}
+
+export default connect(mapStateToProps, {
   findUsers,
   getUserInfo,
   getAllFriends,
   getRequest
- })(Search);
+})(Search);
